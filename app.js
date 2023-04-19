5330630474f4fa168c4b3e63d6943769d25b7a91
Jesse Mrozowski <jesse.mrozowski@gmail.com>
2023-04-19 16:44:05 -0700

    Adding Zoidberg to webpage

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 'use strict';
 
-const response = confirm("Do you like Zoidberg?");
-
-if(response == true)
+if(confirm("Do you like Zoidberg?"))
 {
     <html><img src="Zoidberg-Jesus.jpg" alt="You have good taste."></img></html>
 }
 else
 {
-    <html><img src="why-not-zoidberg-a3d22c7060.jpg" alt="You have good taste."></img></html>
+    <html><img src="why-not-zoidberg-a3d22c7060.jpg" alt="We're ashamed of you"></img></html>
 }
