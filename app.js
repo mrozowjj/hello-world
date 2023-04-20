48653fa50d1129e456c20245590ab060ff818f4f
Jesse Mrozowski <jesse.mrozowski@gmail.com>
2023-04-20 09:31:15 -0700

    Cleaning up the page

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 
 if(confirm("Do you like Zoidberg?"))
 {
-    <html><img src="Zoidberg-Jesus.jpg" alt="You have good taste."></img></html>
+    document.write('<html><img src="Zoidberg-Jesus.jpg" alt="You have good taste."></img></html>')
 }
 else
 {
-    <html><img src="why-not-zoidberg-a3d22c7060.jpg" alt="We're ashamed of you"></img></html>
+    document.write('<html><img src="why-not-zoidberg-a3d22c7060.jpg" alt="We\'re ashamed of you"></img></html>')
 }
