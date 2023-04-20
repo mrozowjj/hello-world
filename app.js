e8da15984ecbe967b203af2826e73c3e9df2424c
Jesse Mrozowski <jesse.mrozowski@gmail.com>
2023-04-20 11:51:03 -0700

    Adding Functions

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 'use strict';
 
 
+function printDate()
+{
+    let currentdate = new Date();
+    return document.write(currentdate);
+}
 
-if(confirm("Do you like Zoidberg?"))
+function lightDark()
 {
-    document.write('<html><img src="Zoidberg-Jesus.jpg" alt="You have good taste."></img></html>')
+    const StyleMode = prompt("Light or dark mode?");
+
+    // if(stylePreference == 'dark')
+    // {
+    
+    // }
+    // else
+    // {
+    
+    // }
+
+    return StyleMode;
 }
-else
+
+function zoidberg()
 {
-    document.write('<html><img src="why-not-zoidberg-a3d22c7060.jpg" alt="We\'re ashamed of you"></img></html>')
+
+    if(confirm("Do you like Zoidberg?"))
+    {
+        document.write('<html><img src="Zoidberg-Jesus.jpg" alt="You have good taste."></img></html>')
+    }
+    else
+    {
+        document.write('<html><img src="why-not-zoidberg-a3d22c7060.jpg" alt="We\'re ashamed of you"></img></html>')
+    }
+
+    return;
 }
+
+
